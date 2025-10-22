45d9ca77706f8eadfa5cfa8fbc18a6629535b432
Aparna <madanuru.aparna@gmail.com>
2025-10-23 02:41:08 +0530

    minor issue fixed.

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 const AdminRoute = ({children}) =>{
     debugger
-    const {user, setRedirectPath, loading} = useAuth();
+    // const {user, setRedirectPath, loading} = useAuth();
+const user = JSON.parse(localStorage.getItem("user"));
     if(!user){
         return <Navigate to="/login" />
     }
